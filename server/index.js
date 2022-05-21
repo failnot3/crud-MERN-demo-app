8ae235904ec4744fe1da2548202c5bab9401b0f0
theDTS1 <orel1one@gmail.com>
2022-05-21 14:28:22 +0300

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+const FoodModel = require("./models/Food");
+
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://dts:sUdbu1Kh9WPGw1NS@cruddemo.e69mb.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.get("/", async (req, res) => {
+  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+
+  try {
+    await food.save();
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(3001, () => {
   console.log("Server running on port 3001 ...");
