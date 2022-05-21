71c0790e9f14c29917e1ad56399b956ee27cbaa6
theDTS1 <orel1one@gmail.com>
2022-05-21 15:26:24 +0300

    ver1-update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const cors = require("cors");
+
 const app = express();
 
 const FoodModel = require("./models/Food");
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://dts:sUdbu1Kh9WPGw1NS@cruddemo.e69mb.mongodb.net/food?retryWrites=true&w=majority",
@@ -13,8 +17,10 @@ mongoose.connect(
   }
 );
 
-app.get("/", async (req, res) => {
-  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+app.post("/insert", async (req, res) => {
+  const foodName = req.body.foodName;
+  const days = req.body.days;
+  const food = new FoodModel({ foodName: foodName, daysSinceIAte: days });
 
   try {
     await food.save();
